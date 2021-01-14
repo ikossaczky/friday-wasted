fb1998d260674e5697fd7b8be813bc7bb3c802c5
Igor Kossaczky <ikossaczky@gmail.com>
2021-01-14 22:17:00 +0100

    extension works.

--- background/friday_wasted.js
+++ background/friday_wasted.js
@@ -1,52 +1,72 @@
 // Initialize the list of blocked hosts
 let blockedDomainsArray = ["example.com", "example.org"];
 let blockedDomainsRegex = regex_from_array(blockedDomainsArray)
-console.log(`balblablaaa!!!`);
+let dayArray = [false, false, false, false, false, true, false]
+let time="20:00"
+let minutes_threshold = int_from_time(time)
+//console.log(`blablablaaa!!!`);
 
 function regex_from_array(domainsArray) {
-  return  new RegExp("(.*"+domainsArray.join('.*)|(.*').replace('.','\\.')+".*)", "i")
+  return  new RegExp(("(.*"+domainsArray.join('.*)|(.*').replace('.','\\.')+".*)").replace("|(.*.*)",""), "i")
 }
+function int_from_time(time) {
+  return parseInt(time.substr(0,2))*60 + parseInt(time.substr(3,4))
+}
+  
 
 
 // Set the default list on installation.
 browser.runtime.onInstalled.addListener(details => {
   browser.storage.local.set({
     blockedDomainsArray: blockedDomainsArray,
+    dayArray: dayArray,
+    time: time
   });
 });
 
 // Get the stored regex
 browser.storage.local.get(data => {
   if (data.blockedDomainsArray) {
-    console.log(`storage get0`);
+    //console.log(`storage get0`);
+    //console.log(data)
     blockedDomainsArray = data.blockedDomainsArray
     blockedDomainsRegex = regex_from_array(blockedDomainsArray);
-    console.log(`storage get1`);
+    time = data.time
+    minutes_threshold = int_from_time(time)
+    dayArray = data.dayArray
+    //console.log(`storage get1`);
   }
 });
 
 // Listen for changes in the blocked list
 browser.storage.onChanged.addListener(changeData => {
-  console.log(`storage changed0`);
+  //console.log(`storage changed0`);
+  //console.log(changeData)
   blockedDomainsArray = changeData.blockedDomainsArray.newValue
   blockedDomainsRegex = regex_from_array(blockedDomainsArray);
+  time = changeData.time.newValue
+  minutes_threshold = int_from_time(time)
+  dayArray = changeData.dayArray.newValue
   console.log(`storage changed1`);
 });
 
 
 function listener(requestInfo) {
   const url = new URL(requestInfo.url);
+  var d = new Date()
+  day = d.getDay()
+  var current_minutes = d.getHours()*60 + d.getMinutes()
   console.log(`listening...${url.hostname}`);
   if (blockedDomainsRegex.test(url.hostname)) {
-    console.log(`its blocked`);
-  return {redirectUrl: "http://ikossaczky.github.io/"};
+    if (!dayArray[day] || current_minutes<minutes_threshold) {
+      console.log(`its blocked`);
+      return {redirectUrl: "http://ikossaczky.github.io/"};
+    }
   }
   return {};
 };
 
 // Listen for a request to open a webpage
-// browser.webRequest.onBeforeRequest.addListener(listener, {urls: ["<all_urls>"], types: ["main_frame"]},["blocking"]);
-
 browser.webRequest.onBeforeRequest.addListener(
   listener,
   {urls: ["<all_urls>"]},
--- options/options.js
+++ options/options.js
@@ -1,10 +1,25 @@
-const blockedDomainsTextArea = document.querySelector("#blocked-hosts");
+const blockedDomainsTextArea = document.getElementById("blocklist_elem");
+var i;
+const dayCheckboxArray = []
+for (i = 0; i < 7; i++) {
+  dayCheckboxArray.push(document.getElementById("day"+i.toString()+"_elem"))
+} 
+const timeEntry = document.getElementById("time_elem");
+const htmlElemArray = dayCheckboxArray.concat([blockedDomainsTextArea, timeEntry])
 
 // Store the currently selected settings using browser.storage.local.
 function storeSettings() {
   let blockedDomainsArray = blockedDomainsTextArea.value.split("\n");
+  let dayArray = []
+  let i;
+  for (i = 0; i < dayCheckboxArray.length; i++) {
+    dayArray[i] = dayCheckboxArray[i].checked
+  } 
+  let time = timeEntry.value
   browser.storage.local.set({
     blockedDomainsArray: blockedDomainsArray,
+    dayArray: dayArray,
+    time: time
   });
 }
 
@@ -12,6 +27,11 @@ function storeSettings() {
 // or the default settings if the stored settings are empty.
 function updateUI(restoredSettings) {
   blockedDomainsTextArea.value = restoredSettings.blockedDomainsArray.join("\n");
+  let i;
+  for (i = 0; i < dayCheckboxArray.length; i++) {
+    dayCheckboxArray[i].checked = restoredSettings.dayArray[i]
+  } 
+  timeEntry.value = restoredSettings.time
 }
 
 function onError(e) {
@@ -22,4 +42,8 @@ function onError(e) {
 browser.storage.local.get().then(updateUI, onError);
 
 // Whenever the contents of the textarea changes, save the new values
-blockedDomainsTextArea.addEventListener("change", storeSettings);
+//blockedDomainsTextArea.addEventListener("change", storeSettings);
+var i;
+for (i = 0; i < htmlElemArray.length; i++) {
+  htmlElemArray[i].addEventListener("change", storeSettings);
+} 
